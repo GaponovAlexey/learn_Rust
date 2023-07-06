82e23a2f2e0b8c4f4f6fe8eb1ed621226d644513
Kekelberi <alexei.gaponov@gmail.com>
2023-07-06 04:57:11 -0500

    save

---
+++ learnRust/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let name: String = String::from("Bob");
+    let age: f32 = f32::from(3.14);
+
+    print!("my args = {}, {}", age, name);
+    Lear();
+}
+
+fn Lear() {
+    let x = 5;
+    println!("{}", x)
+}
