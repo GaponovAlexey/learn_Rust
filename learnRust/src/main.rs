d7c19a54829047ef109383e3aa84a0b2c890b860
Kekelberi <alexei.gaponov@gmail.com>
2023-07-08 17:28:31 -0500

    save

--- learnRust/src/main.rs
+++ learnRust/src/main.rs
@@ -1,29 +1,9 @@
-#[derive(Debug)]
-enum Types {
-    Int(i32),
-    Float(f64),
-    Bool(bool),
-    Text(String),
-}
 fn main() {
-    let list: Vec<Types> = vec![
-        Types::Int(8),
-        Types::Float(2.2),
-        Types::Bool(true),
-        Types::Text("ds".to_string()),
-    ];
-    match &list[1] {
-        Types::Int(el) => {
-            println!("{}", el);
-        }
-        Types::Float(el) => {
-            println!("{}", el);
-        }
-        Types::Bool(el) => {
-            println!("{}", el);
-        }
-        Types::Text(el) => {
-            println!("{}", el);
-        }
+    let s1 = "la".to_string();
+    let s2 = "w".to_string();
+    for el in s1.bytes() {
+        println!("{}", el);
     }
+
+    println!("{}", &s1[..2]);
 }
