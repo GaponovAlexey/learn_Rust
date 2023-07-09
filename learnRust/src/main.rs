ae7fcf6f086c4a74ee528b40c222d42a3c1ec4bd
Kekelberi <alexei.gaponov@gmail.com>
2023-07-08 20:58:17 -0500

    save

--- learnRust/src/main.rs
+++ learnRust/src/main.rs
@@ -1,9 +1,13 @@
+use std::collections::HashMap;
+
 fn main() {
-    let s1 = "la".to_string();
-    let s2 = "w".to_string();
-    for el in s1.bytes() {
-        println!("{}", el);
+    let s = "Learn learn Rust with with me me me".to_lowercase();
+    let mut count_map = HashMap::new();
+
+    for s in s.split_whitespace() {
+        let count = count_map.entry(s).or_insert(0);
+        *count += 1;
     }
 
-    println!("{}", &s1[..2]);
+    println!("{:?}",count_map);
 }
