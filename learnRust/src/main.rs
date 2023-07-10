0d71ac9f73fa5c8c2e3ffb6e95ca57dbdb12b89b
Kekelberi <alexei.gaponov@gmail.com>
2023-07-10 18:37:42 -0500

    save

--- learnRust/src/main.rs
+++ learnRust/src/main.rs
@@ -1,9 +1,24 @@
-use std::fs::File;
-
+use std::{
+    fs::File,
+    io::{self, ErrorKind, Read},
+};
 
 fn main() {
-    let path = "lol.txt";
-    let f = File::open(path).expect("my err");
-   
-    
+    let path = "text.txt";
+    match read_file(path) {
+        Ok(data) => println!("result in file => {:?}", data),
+        Err(e) => match e.kind() {
+            ErrorKind::NotFound => match File::create(path) {
+                Ok(_) => println!("create file"),
+                Err(e) => panic!("{}", e),
+            },
+            other => panic!("my Error {}", other),
+        },
+    };
+}
+
+fn read_file(path: &str) -> io::Result<String> {
+    let mut data = String::new();
+    File::open(path)?.read_to_string(&mut data)?;
+    Ok(data)
 }
