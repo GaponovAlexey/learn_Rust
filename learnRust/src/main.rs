99819d5cebebb78792e43ffd5aa1d2a7c18a1861
Kekelberi <alexei.gaponov@gmail.com>
2023-07-09 19:36:04 -0500

    save

--- learnRust/src/main.rs
+++ learnRust/src/main.rs
@@ -1,14 +1,9 @@
-use std::collections::HashMap;
+use std::fs::File;
+
 
 fn main() {
-    let s = "Learn learn Rust with with me me me".to_lowercase();
-    let mut count_map = HashMap::new();
-
-    for s in s.split_whitespace() {
-        let count = count_map.entry(s).or_insert(0);
-        // println!("{}", &count);
-        *count += 1;
-    }
-
-    println!("{:?}", count_map);
+    let path = "lol.txt";
+    let f = File::open(path).expect("my err");
+   
+    
 }
