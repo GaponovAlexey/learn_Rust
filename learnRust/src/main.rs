6d963096828ca340624861068631d8aa0c8b8cb2
Kekelberi <alexei.gaponov@gmail.com>
2023-07-11 12:50:58 -0500

    save

--- learnRust/src/main.rs
+++ learnRust/src/main.rs
@@ -1,17 +1,19 @@
 fn main() {
     let nums = [1, 2, 3, 4, 4, 3, 5, 5, 5, 6, 6, 7, 7, 9, 10];
-    println!("{:?}", find_duplicate(&nums));
+    let res = find_duplicate(&nums);
+    println!("{:?}", res);
 }
 
 fn find_duplicate(list: &[i32]) -> Vec<i32> {
-    let mut dup: Vec<i32> = Vec::new();
+    let mut dup = Vec::new();
 
-    for i in 0..(list.len()) {
-        for j in (i + 1)..(list.len()) {
-            if list[i] == list[j] {
+    for i in 0..list.len() {
+        for j in i + 1..list.len() {
+            if list[i] == list[j] && !dup.contains(&list[i]) {
                 dup.push(list[i]);
             }
         }
     }
+
     dup
 }
