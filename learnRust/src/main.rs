3aca156a6f2fd1ee9aa22ed4bc3ef06d4dabf23c
Kekelberi <alexei.gaponov@gmail.com>
2023-07-16 12:34:15 -0500

    save

--- learnRust/src/main.rs
+++ learnRust/src/main.rs
@@ -1,4 +1,6 @@
-use chrono::{ DateTime, Local };
+extern crate chrono;
+use chrono::prelude::*;
+
 enum Priority {
     Low,
     Medium,
@@ -27,13 +29,62 @@ impl Task {
     }
     fn print_task(&self) {
         println!(
-            "name:{:?}desk{:?}priority{:?}time{:?}",
+            "{} | {} | {:?}\n\"{}\"\n",
             self.name,
-            self.description,
             self.priority.to_string(),
-            self.add_time.format("%d-%m-%Y %H:%M:%S %z")
+            self.add_time.format("%d-%m-%Y %H:%M:%S %z").to_string(),
+            self.description
         )
     }
 }
 
-fn main() {}
+struct TasksManager {
+    tasks: Vec<Task>,
+}
+
+impl TasksManager {
+    fn new() -> Self {
+        Self { tasks: vec![] }
+    }
+
+    fn print_task(&self) {
+        for task in &self.tasks {
+            println!("trrrr");
+            task.print_task();
+        }
+    }
+
+    fn add_task(&mut self, task: Task) {
+        self.tasks.push(task)
+    }
+
+    fn find_task(&self, name: String) -> Option<usize> {
+        self.tasks.iter().position(|el| el.name == name)
+    }
+
+    fn remove_task(&mut self, name: String) -> Result<String, String> {
+        if let Some(i) = self.find_task(name) {
+            self.tasks.remove(i);
+            Ok(format!("Task {} removed", name))
+        } else {
+            Err(format!("Task \"{}\" doesn't exist", name))
+        }
+    }
+    fn edit_task(&mut self, name: String, update_task: Task) -> Result<String, String> {
+        if let Some(i) = self.find_task(name) {
+            if let Some(task) = self.tasks.get_mut(i) {
+                task.name = update_task.name;
+                Ok(format!("Task {} removed", name))
+            } else {
+                Ok(format!("Task {} removed", name))
+            }
+        } else {
+            Err(format!("Task \"{}\" doesn't exist", name))
+        }
+    }
+}
+
+fn main() {
+    let mut task = Task::new("my_task".to_string(), "i Need it...".to_string(), Priority::High);
+    // task.print_task();
+}
